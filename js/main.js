f3544cf763f8f9b60076aebe2592f9a186019b79
ifeyi <ifeyibatindek@gmail.com>
2013-10-01 17:47:19 +0100

    Modification fichiers JS
    petit pb de scroll résolu dans le fichier main.js, styles modifiés dans
    les fichiers grid.css & main.css

--- js/main.js
+++ js/main.js
@@ -24,4 +24,12 @@ $(window).load(function(){
 	  $("html, body").animate({ scrollTop: 0 }, "slow");
 	  return false;
   });
+  
+  $(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('.scrollTo').fadeIn();
+    } else {
+        $('.scrollTo').fadeOut();
+    }
+  });
 });
